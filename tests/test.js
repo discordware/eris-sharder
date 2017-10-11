610cdac905cbc98f061d49cdb61e5deb22c39739
MrJohnCoder <johnsmithcoder7@gmail.com>
2017-10-11 07:14:35 -0400

    Create test.js

---
+++ tests/test.js
@@ -0,0 +1,17 @@
+const Sharder = require("../");
+
+let sharder = new Sharder("test", "/main.js", {
+name: "Travis CLI",
+stats: true,
+clusters: 2,
+shards: 4,
+debug: true
+});
+
+sharder.on("stats", stats => {
+console.log(stats)
+});
+
+setTimeout(() => {
+process.exit();
+}, 1000 * 60 * 2);
